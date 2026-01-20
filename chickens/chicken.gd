fdaa804b10fdbee3152a323b9fe62036617e8b86
mrbartchak <matthewbartchak@gmail.com>
2026-01-19 22:37:26 -0800

    idk why these not get added

--- chickens/chicken.gd
+++ chickens/chicken.gd
@@ -7,6 +7,22 @@ var health: int
 func _ready() -> void:
 	health = max_health
 
+# ===================
+#      Movement
+# ===================
+var start_position: Vector2
+var target_position: Vector2
+
+func pick_new_target():
+	var wander_radius: float = 20.0
+	var offset = Vector2(
+		randf_range(-wander_radius, wander_radius),
+		randf_range(-wander_radius, wander_radius)
+	)
+	target_position = start_position + offset
+# ===================
+#       Damage
+# ===================
 func take_damage(amount: int) -> void:
 	health -= amount
 	flash()
@@ -15,9 +31,9 @@ func take_damage(amount: int) -> void:
 
 func flash() -> void:
 	var flash_time: float = 0.08
-	$Sprite2D.modulate = Color(5, 5, 5,)
+	$Sprite2D.modulate = Color(10, 10, 10)
 	await get_tree().create_timer(flash_time).timeout
-	$Sprite2D.modulate = Color(1, 1, 1, 1)
+	$Sprite2D.modulate = Color(1, 1, 1)
 
 func die() -> void:
 	queue_free()
