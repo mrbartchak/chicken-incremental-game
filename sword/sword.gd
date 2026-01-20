eee51d714c92f00712ac418893eb8150384ddd5b
mrbartchak <matthewbartchak@gmail.com>
2026-01-19 22:37:08 -0800

    tons bro tons, some more sprites, mostly

--- sword/sword.gd
+++ sword/sword.gd
@@ -2,6 +2,7 @@ class_name Sword
 extends Area2D
 
 var follow_speed: float = 32.0
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var attack_timer: Timer = $AttackTimer
 
 func _ready() -> void:
@@ -23,11 +24,13 @@ func attack() -> void:
 			area.take_damage(1)
 
 func play_swing() -> void:
-	var _swing_scale: Vector2 = Vector2(1.5, 1.5)
+	$SwingSound.pitch_scale = randf_range(0.6, 1.4)
+	$SwingSound.play()
+	var swing_scale: Vector2 = Vector2(1.5, 1.5)
 	var swing_time: float = 0.08
-	var swing_rotation: float = deg_to_rad(-60)
-	#$Sprite2D.scale = swing_scale
-	$Sprite2D.rotation = swing_rotation
+	var swing_rotation: float = deg_to_rad(-90)
+	$Sprite2D.scale = swing_scale
+	sprite.rotation = swing_rotation
 	await get_tree().create_timer(swing_time).timeout
-	#$Sprite2D.scale = Vector2.ONE
-	$Sprite2D.rotation = 0.0
+	$Sprite2D.scale = Vector2.ONE
+	sprite.rotation = 0.0
