c3921ac273b75f8408cbb8a5e90e181fbf568ec9
mrbartchak <matthewbartchak@gmail.com>
2026-01-28 14:55:49 -0800

    fixing some canvas layer ordering for shop menu and cursor

--- core/autoloads/gamestate.gd
+++ core/autoloads/gamestate.gd
@@ -3,11 +3,15 @@ extends Node
 signal wings_changed(amount: int)
 signal chicken_count_changed(amount: int)
 
-var chicken_count: int = 0
 var wings: int = 0
+var total_wings_collected: int = 0
+
+var chicken_count: int = 0
+var max_chickens: int = 1
 
 func add_wings(amount: int = 1) -> void:
 	wings += amount
+	total_wings_collected += amount
 	wings_changed.emit(wings)
 
 func add_chickens(amount: int = 1) -> void:
--- ui/menu/shop/shop_menu.gd
+++ ui/menu/shop/shop_menu.gd
@@ -1,5 +1,5 @@
 class_name ShopMenu
-extends Control
+extends CanvasLayer
 
 func _ready() -> void:
 	close()
