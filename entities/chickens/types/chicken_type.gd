3f0b20f99c6e9c4a59ab90b51c8508c73bf2b9f4
mrbartchak <matthewbartchak@gmail.com>
2026-02-09 12:03:38 -0800

    silver chicken sprite frames and type

--- entities/chickens/types/chicken_type.gd
+++ entities/chickens/types/chicken_type.gd
@@ -1,8 +1,9 @@
 class_name ChickenType
 extends Resource
 
+@export var id: String
 @export var display_name: String
 @export var sprite_frames: SpriteFrames
-@export var wing_multiplier: float = 1.0
 @export var speed: float = 50.0
 @export var health: int = 1
+@export var wing_type: WingType
