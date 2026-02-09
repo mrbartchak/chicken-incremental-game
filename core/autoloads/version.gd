6757158e5a503bef284106f256aed3d27c8c0ef6
mrbartchak <matthewbartchak@gmail.com>
2026-02-08 22:06:36 -0800

    changelog and verison

--- core/autoloads/version.gd
+++ core/autoloads/version.gd
@@ -2,9 +2,9 @@ extends Node
 
 const MAJOR = 0
 const MINOR = 2
-const PATCH = 1
+const PATCH = 2
 
-const VERSION_STRING = "0.2.1"
+const VERSION_STRING = "0.2.2"
 
 func get_version() -> String:
 	return VERSION_STRING
