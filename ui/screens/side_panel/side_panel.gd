5e80db158eb47fa3a7e60b8b9418234e275c7d68
mrbartchak <matthewbartchak@gmail.com>
2026-02-14 01:51:42 -0800

    just played the game through lol

--- ui/screens/side_panel/side_panel.gd
+++ ui/screens/side_panel/side_panel.gd
@@ -19,13 +19,23 @@ func _ready() -> void:
 	shop_button.pressed.connect(_show_panel.bind(panels[2]))
 	settings_button.pressed.connect(_show_panel.bind(panels[3]))
 	
-	
-	
 	_show_panel(panels[0])
 	upgrades_button.grab_focus()
+	
+	_handle_settings()
 
 func _show_panel(panel_to_show: PanelContainer) -> void:
 	for panel in panels:
 		panel.hide()
 	
 	panel_to_show.show()
+
+func _handle_settings() -> void:
+	var menu_button: Button = $MarginContainer/VBoxContainer/SettingsPanel/SettingsMargin/VBoxContainer/MenuButton
+	var quit_button: Button = $MarginContainer/VBoxContainer/SettingsPanel/SettingsMargin/VBoxContainer/QuitButton
+	menu_button.pressed.connect(func(): 
+		get_tree().change_scene_to_file("res://ui/screens/main_menu/main_menu.tscn")
+	)
+	quit_button.pressed.connect(func():
+		get_tree().quit()
+	)
